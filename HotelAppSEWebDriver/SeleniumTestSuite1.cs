4e616cf1961c37613f41531db26b7eb8f24b68da
Antal Nagy <antal.nagy@invenias.com>
2019-03-04 15:08:20 +0000

    more tests

--- HotelAppSEWebDriver/SeleniumTestSuite1.cs
+++ HotelAppSEWebDriver/SeleniumTestSuite1.cs
@@ -102,7 +102,65 @@ namespace SeleniumTests
         }
 
         [Test]
-        public void RegisterUser002()
+        public void BokkedItinerary002()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.XPath("//*[@id=\"username\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"username\"]")).Clear();
+            driver.FindElement(By.XPath("//*[@id=\"username\"]")).SendKeys("makrobaktat");
+            driver.FindElement(By.XPath("//*[@id=\"password\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"password\"]")).Clear();
+            driver.FindElement(By.XPath("//*[@id=\"password\"]")).SendKeys("P455w0rd!");
+            driver.FindElement(By.XPath("//*[@id=\"login\"]")).Click();
+            Assert.IsTrue(IsElementPresent(By.LinkText("Logout")));
+            driver.FindElement(By.XPath("//*[@id=\"location\"]")).Click();
+            new SelectElement(driver.FindElement(By.XPath("//*[@id=\"location\"]"))).SelectByText("Sydney");
+            driver.FindElement(By.XPath("//option[@value='Sydney']")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"room_nos\"]")).Click();
+            new SelectElement(driver.FindElement(By.XPath("//*[@id=\"room_nos\"]"))).SelectByText("2 - Two");
+            driver.FindElement(By.XPath("//option[@value='2']")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"adult_room\"]")).Click();
+            new SelectElement(driver.FindElement(By.XPath("//*[@id=\"adult_room\"]"))).SelectByText("2 - Two");
+            driver.FindElement(By.XPath("(//option[@value='2'])[2]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"Submit\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"radiobutton_2\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"continue\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"first_name\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"first_name\"]")).Clear();
+            driver.FindElement(By.XPath("//*[@id=\"first_name\"]")).SendKeys("Baktat");
+            driver.FindElement(By.XPath("//*[@id=\"last_name\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"last_name\"]")).Clear();
+            driver.FindElement(By.XPath("//*[@id=\"last_name\"]")).SendKeys("Makroniciusz");
+            driver.FindElement(By.XPath("//*[@id=\"address\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"address\"]")).Clear();
+            driver.FindElement(By.XPath("//*[@id=\"address\"]")).SendKeys("1 London Road");
+            driver.FindElement(By.XPath("//*[@id=\"cc_num\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cc_num\"]")).Clear();
+            driver.FindElement(By.XPath("//*[@id=\"cc_num\"]")).SendKeys("1234567899876543");
+            driver.FindElement(By.XPath("//*[@id=\"cc_type\"]")).Click();
+            new SelectElement(driver.FindElement(By.XPath("//*[@id=\"cc_type\"]"))).SelectByText("American Express");
+            driver.FindElement(By.XPath("//*[@id=\"cc_exp_month\"]")).Click();
+            new SelectElement(driver.FindElement(By.XPath("//*[@id=\"cc_exp_month\"]"))).SelectByText("June");
+            driver.FindElement(By.XPath("//option[@value='6']")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cc_exp_year\"]")).Click();
+            new SelectElement(driver.FindElement(By.XPath("//*[@id=\"cc_exp_year\"]"))).SelectByText("2021");
+            driver.FindElement(By.XPath("//option[@value='2021']")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cc_cvv\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cc_cvv\"]")).Clear();
+            driver.FindElement(By.XPath("//*[@id=\"cc_cvv\"]")).SendKeys("123");
+            driver.FindElement(By.XPath("//*[@id=\"book_now\"]")).Click();
+            var a = driver.FindElement(By.Id("order_no")).GetAttribute("value").ToString();
+            driver.FindElement(By.LinkText("Booked Itinerary")).Click();
+            driver.FindElement(By.Id("order_id_text")).Clear();
+            driver.FindElement(By.Id("order_id_text")).SendKeys(a);
+            driver.FindElement(By.Id("search_hotel_id")).Click();
+            var b = driver.FindElement(By.XPath("(//input[contains(@type,'text')])[3]")).GetAttribute("value").ToString();
+            Assert.AreEqual(a, b);
+
+        }
+
+        [Test]
+        public void RegisterUser003()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.XPath("/html/body/table[2]/tbody/tr/td[2]/form/table/tbody/tr[7]/td/a")).Click();
@@ -119,11 +177,12 @@ namespace SeleniumTests
             driver.FindElement(By.Id("tnc_box")).Click();
             driver.FindElement(By.Id("Submit")).Click();
             Assert.AreEqual("Captcha is Empty", driver.FindElement(By.Id("captcha_span")).Text);
+            driver.FindElement(By.LinkText("Go back to Login page")).Click();
 
         }
 
         [Test]
-        public void RegisterUserResetButton003()
+        public void RegisterUserResetButton004()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.XPath("/html/body/table[2]/tbody/tr/td[2]/form/table/tbody/tr[7]/td/a")).Click();
@@ -147,6 +206,7 @@ namespace SeleniumTests
             Assert.AreEqual("Email Address is Empty", driver.FindElement(By.Id("email_add_span")).Text);
             Assert.AreEqual("Captcha is Empty", driver.FindElement(By.Id("captcha_span")).Text);
             Assert.AreEqual("You must agree to Terms and Conditions", driver.FindElement(By.Id("tnc_span")).Text);
+            driver.FindElement(By.LinkText("Go back to Login page")).Click();
 
 
         }
